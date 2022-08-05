ad21ca81bef6400370453bbf5b16ffffd7ddb98c
Nazar Lyubuska <nazar.lyubuska@gmail.com>
2022-08-05 20:11:42 +0300

    push 1 exercise

---
+++ src/js/01-gallery.js
@@ -0,0 +1,41 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+new SimpleLightbox('.gallery a', {captionDelay: 250});
+
+const gallery = document.querySelector('.gallery');
+const images = createGalleryItems(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', images);
+
+gallery.addEventListener('click', modalWindow);
+
+function createGalleryItems(items) {
+  return items
+  .map(({preview, original, description}) => {
+    return `
+      <div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+      <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+      </a>
+      </div>`
+  }).join("")
+};
+
+function modalWindow(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+};
